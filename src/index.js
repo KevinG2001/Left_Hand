35c2f48879859dc2cd756d4f84be22ade40f51a7
Kevin Glennon <116925564+KevinG2001@users.noreply.github.com>
2024-08-14 18:40:47 +0100

    Testing deployment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const { Client, Collection, IntentsBitField } = require("discord.js");
-const keep_alive = require("../keep_alive");
+const keep_alive = require("./keep_alive");
 require("dotenv").config();
 const path = require("path");
 const fs = require("fs");
